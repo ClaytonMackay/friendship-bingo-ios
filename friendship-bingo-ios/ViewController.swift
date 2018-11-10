6e1041bff837363753316feb666ab84e1d81bd16
Ethan Abrams <ethana56@Ethans-MacBook-Pro.local>
2018-11-10 13:25:34 -0800

    First commit

---
+++ friendship-bingo-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  friendship-bingo-ios
+//
+//  Created by Ethan Abrams on 11/10/18.
+//  Copyright © 2018 Hack4Impact Cal Poly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
